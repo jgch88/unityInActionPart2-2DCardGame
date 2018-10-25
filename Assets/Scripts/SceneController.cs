32d7348d00621609d455e739e925a36835249595
jgch88 <radium.laboratories@gmail.com>
2018-10-25 14:33:47 +0800

    done section 5.3.3

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -3,12 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneController : MonoBehaviour {
+	// grid spacing
+	public const int gridRows = 2;
+	public const int gridCols = 4;
+	public const float offsetX = 2f;
+	public const float offsetY = 2.5f;
+
 	[SerializeField] private MemoryCard originalCard; // the one card in the scene
 	[SerializeField] private Sprite[] images; // references to sprite assets, you can serialize an Array!
 
 	void Start () {
-		int id = Random.Range (0, images.Length); // minimum value is inclusive, but return value is always below maximum
-		originalCard.SetCard (id, images [id]); // using the card API to change the base card on Start()
+		Vector3 startPos = originalCard.transform.position; // everything is relative to/offset from this base card
+
+		for (int i = 0; i < gridCols; i++) {
+			for (int j = 0; j < gridRows; j++) {
+				MemoryCard card;
+				if (i == 0 && j == 0) {
+					card = originalCard;
+				} else {
+					card = Instantiate (originalCard) as MemoryCard; // cloning
+				}
+
+				int id = Random.Range (0, images.Length);
+				// setting the original card/clones
+				card.SetCard (id, images [id]);
+
+				float posX = (offsetX * i) + startPos.x;
+				float posY = -(offsetY * j) + startPos.y;
+				card.transform.position = new Vector3 (posX, posY, startPos.z);
+			}
+		}
 	}
 	
 	// Update is called once per frame
