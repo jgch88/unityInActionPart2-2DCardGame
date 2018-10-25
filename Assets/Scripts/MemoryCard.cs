9b093a9b5c3e95ed37985dbbd69d86dfc8e989c9
jgch88 <radium.laboratories@gmail.com>
2018-10-25 13:55:49 +0800

    done section 5.2.1, 5.2.2, 5.2.3

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+	[SerializeField] private GameObject cardBack;
+
+	public void OnMouseDown() {
+		// Debug.Log ("Testing clicks");
+		if (cardBack.activeSelf) {
+			cardBack.SetActive (false);
+		}
+	}
+}
