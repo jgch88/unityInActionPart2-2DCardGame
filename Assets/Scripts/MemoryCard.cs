a726f93c1b08f25acb89073930a5f55ab891c62d
jgch88 <radium.laboratories@gmail.com>
2018-10-25 14:04:31 +0800

    done section 5.3.1

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MemoryCard : MonoBehaviour {
 	[SerializeField] private GameObject cardBack;
+	[SerializeField] private Sprite image;
+
+	void Start() {
+		GetComponent<SpriteRenderer> ().sprite = image;
+	}
 
 	public void OnMouseDown() {
 		// Debug.Log ("Testing clicks");
