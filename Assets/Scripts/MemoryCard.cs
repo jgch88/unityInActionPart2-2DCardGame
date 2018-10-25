abb72572637030a94e56e6c65c189df1b0fcbb4f
jgch88 <radium.laboratories@gmail.com>
2018-10-25 14:06:17 +0800

    done section 5.3.1

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -7,6 +7,10 @@ public class MemoryCard : MonoBehaviour {
 	[SerializeField] private Sprite image;
 
 	void Start() {
+		// The Sprite Renderer component is what makes a 2D sprite a sprite
+		// Its main property is "Sprite", which can be set programmatically.
+		// Here we are attaching a sprite into the SerializedField of the script,
+		// which then passes it on to the Sprite Renderer
 		GetComponent<SpriteRenderer> ().sprite = image;
 	}
 
